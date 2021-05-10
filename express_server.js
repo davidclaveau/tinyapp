7425ef0f26728db7b333df5dc506a5d577f760f9
David Claveau <daveclaveau@gmail.com>
2021-05-10 22:17:18 +0000

    Added view for adding a new URL, as well as function to create a random 6-character alphanumeric string.

--- express_server.js
+++ express_server.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+
+function generateRandomString() {
+  let returnValue = "";
+  const randomCharacter = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+  for (let i = 0; i < 6; i++) {
+    returnValue += randomCharacter[Math.floor(Math.random() * 61)];
+  }
+
+ return returnValue;
+};
 
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
@@ -16,6 +28,15 @@ app.get('/', (req, res) => {
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
+});
+
+app.post('/urls', (req, res) => {
+  console.log(generateRandomString());
+  res.send('Ok');
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
