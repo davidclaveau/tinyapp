0aca0257a3560b2889f6733d944c0cde69315880
David Claveau <daveclaveau@gmail.com>
2021-05-11 18:49:05 +0000

    Added functionality to push new URLs and their shortURL to the 'database' and redirect the user to the shortURLs page. Also enabled a redirect when entering the shortURL, which takes the user to the longURL.

--- express_server.js
+++ express_server.js
@@ -6,14 +6,14 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
-function generateRandomString() {
+const generateRandomString = () => {
   let returnValue = "";
   const randomCharacter = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
   for (let i = 0; i < 6; i++) {
     returnValue += randomCharacter[Math.floor(Math.random() * 61)];
   }
 
- return returnValue;
+  return returnValue;
 };
 
 const urlDatabase = {
@@ -25,14 +25,22 @@ app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
 
 app.post('/urls', (req, res) => {
-  console.log(generateRandomString());
-  res.send('Ok');
+  const newShortURL = generateRandomString();
+  const newLongURL = req.body.longURL;
+  urlDatabase[newShortURL] = newLongURL;
+
+  res.redirect(302, `/urls/${newShortURL}`);
 });
 
 app.get("/urls/new", (req, res) => {
@@ -40,7 +48,12 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: 'http://www.lighthouselabs.ca' };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+
+  if (!urlDatabase[req.params.shortURL]) {
+    res.sendStatus(404);
+  }
+  
   res.render("urls_show", templateVars);
 });
 
